53000cbd515566522896dcf3d8f332a9c761622d
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-03-04 13:05:17 +0800

    Basic server

--- client/ftpclient.lua
+++ client/ftpclient.lua
@@ -12,7 +12,7 @@ print("FTP Client v1.0")
 print("\nWelcome to EcliFTP!\nType in the ID of the server you want to connect to.\n\n")
 write("Server ID: ")
 server = read()
-rednet.send(server, "ftp_client_ping")
+rednet.send(server, "ping")
 
 local id,message = rednet.receive(nil, 5)
 clear()
--- server/ftpserver.lua
+++ server/ftpserver.lua
@@ -1,1 +1,12 @@
 print("FTP Server v1.0")
+
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+local port = 21
+modem.open(port)
+
+while true do
+	id, message = rednet.receive()
+	if message == "ping" then
+		rednet.send(id, "pong")
+	end
+end
