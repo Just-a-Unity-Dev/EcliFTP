6c06e4648aa4d0ee645267dffd337eb0fcdeee05
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-03-04 13:02:44 +0800

    Basic FTP client

--- client/ftpclient.lua
+++ client/ftpclient.lua
@@ -1,3 +1,25 @@
 -- This is where the fun begins!
+local function clear()
+	shell.run("clear")
+end
+clear()
+
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+local port = 21
+modem.open(port)
 
 print("FTP Client v1.0")
+print("\nWelcome to EcliFTP!\nType in the ID of the server you want to connect to.\n\n")
+write("Server ID: ")
+server = read()
+rednet.send(server, "ftp_client_ping")
+
+local id,message = rednet.receive(nil, 5)
+clear()
+
+if not id then
+	error("Server timeout, have you turned on the server?")
+else
+	print("Connected to server %d":format(id))
+end
+clear()
